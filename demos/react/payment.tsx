8deca5ac4c8b04d3373f53ab1836c231267882a4
Simon Paris <simon.paris@xendit.co>
2025-12-05 14:01:02 +0800

    feat: Update react demo

---
+++ demos/react/payment.tsx
@@ -0,0 +1,99 @@
+import { useCallback, useLayoutEffect, useRef, useState } from "react";
+import {
+  XenditFatalErrorEvent,
+  XenditSessionSdk,
+  XenditSessionTestSdk,
+} from "xendit-components";
+import { PageType } from "./types";
+
+export const Payment: React.FC<{
+  onSuccess: () => void;
+  onFail: (message: string) => void;
+  goToPage: (page: PageType) => void;
+}> = ({ onSuccess, onFail, goToPage }) => {
+  const el = useRef<HTMLDivElement | null>(null);
+  const sdkRef = useRef<XenditSessionSdk | null>(null);
+
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
+
+  useLayoutEffect(() => {
+    // Using the test SDK class
+    let sdk: XenditSessionSdk;
+    const componentsKey = prompt(
+      "Enter your Xendit Components Key (leave blank to use mock data)",
+    );
+    if (componentsKey === null) {
+      goToPage("store");
+      return;
+    }
+
+    if (componentsKey) {
+      sdk = new XenditSessionSdk({
+        sessionClientKey: componentsKey,
+      });
+    } else {
+      sdk = new XenditSessionTestSdk({});
+    }
+    sdkRef.current = sdk;
+
+    sdk.addEventListener("init", () => {
+      setLoading(false);
+
+      const cards = sdk
+        .getAvailablePaymentChannels()
+        .find((channel) => channel.channelCode === "CARDS");
+      if (cards) {
+        const component = sdk.createPaymentComponentForChannel(cards);
+        el.current?.replaceChildren(component);
+      }
+    });
+
+    sdk.addEventListener("submission-begin", () => {
+      setSubmitting(true);
+    });
+    sdk.addEventListener("submission-end", () => {
+      setSubmitting(false);
+    });
+  }, [goToPage]);
+
+  useLayoutEffect(() => {
+    if (!sdkRef.current) return;
+
+    sdkRef.current?.addEventListener("session-complete", onSuccess);
+    return () => {
+      sdkRef.current?.removeEventListener("session-complete", onSuccess);
+    };
+  }, [onSuccess]);
+
+  useLayoutEffect(() => {
+    if (!sdkRef.current) return;
+
+    function handleError(event: XenditFatalErrorEvent) {
+      onFail(event.message);
+    }
+
+    sdkRef.current?.addEventListener("fatal-error", handleError);
+    return () => {
+      sdkRef.current?.removeEventListener("fatal-error", handleError);
+    };
+  }, [onFail]);
+
+  const onSubmit = useCallback(() => {
+    sdkRef.current?.submit();
+  }, []);
+
+  return (
+    <div>
+      <div className="xendit-component-container" ref={el}></div>
+      <button className="submit" onClick={onSubmit}>
+        Submit Simulated Payment
+      </button>
+      {loading || submitting ? (
+        <div className="loading">
+          <div className="loading-spinner"></div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
