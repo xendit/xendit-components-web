0f78f24707fb0366a02c9181d199cbf749e18cb4
Endy Hardy <endy@xendit.co>
2026-02-03 14:19:14 +0800

    va content

--- sdk/src/components/action-va.tsx
+++ sdk/src/components/action-va.tsx
@@ -1,4 +1,5 @@
 import { TFunction } from "i18next";
+import { amountFormat } from "../amount-format";
 
 type Props = {
   amount: number;
@@ -14,21 +15,37 @@ type Props = {
 
 export function ActionVa(props: Props) {
   const {
-    // amount,
+    amount,
     // channelLogo,
-    // currency,
+    currency,
     // mock,
     // onAffirm,
     vaNumber,
     merchantName,
-    // t,
+    t,
     // title,
   } = props;
 
   return (
-    <div>
-      <div>{vaNumber}</div>
-      <div>{merchantName}</div>
+    <div className="xendit-action-va-content">
+      <div className="xendit-action-va-details">
+        <div className="xendit-action-va-heading xendit-text-12 xendit-text-semibold">
+          {t("action_va.virtual_account_number")}
+        </div>
+        <div className="xendit-action-va-value">{vaNumber}</div>
+        <div className="xendit-action-va-tag xendit-text-12">
+          {merchantName}
+        </div>
+      </div>
+      <hr className="xendit-dotted-line" />
+      <div className="xendit-action-va-details">
+        <div className="xendit-action-va-heading xendit-text-12 xendit-text-semibold">
+          {t("action_va.amount_to_pay")}
+        </div>
+        <div className="xendit-action-va-value">
+          {amountFormat(amount, currency)}
+        </div>
+      </div>
     </div>
   );
 }
